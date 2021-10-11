8e27f4abd22be3b90aa95a65dc472c6451c6d155
JoelJimenezDAW <73123220+JoelJimenezDAW@users.noreply.github.com>
2021-10-11 20:05:20 +0200

    UPADTE

---
+++ MP06/angular/bases/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ContadorComponent } from './contador/contador.component';
+import { HeroeComponent } from './heroes/heroe/heroe.componet';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContadorComponent,
+    HeroeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [HeroeComponent]
+})
+export class AppModule { }
---
+++ MP06/angular/bases/src/app/heroes/heroe/heroe.componet.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'heroe-component',
+    templateUrl: './heroe.component.html'
+})
+
+export class HeroeComponent {
+  title: string = 'Heroe';
+  nombre: string = 'Spiderman';
+  edad: number = 22;
+
+
+
+
+  
+}
+export class AppModule { }
